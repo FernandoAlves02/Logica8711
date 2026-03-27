1c3da8c24f7ac1a174458f44f528cc84e6513a44
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-03-27 20:06:27 -0300

    instrução sobre o uso da biblioteca iomanip com setprecision

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include<iostream>
+#include<iomanip>
 
 int main(){
 
     int a = 8; //int número inteiro
     float b = 9.6; //float número de ponto flutuante "pega até 7 casas decimais" ocupa 4bytes
-    double c = 4.6; //double ocupa 8bytes "pega até mais números pós ponto"
+    double c = 4.6654165413; //double ocupa 8bytes "pega até mais números pós ponto"
 
 
     std::cout<<a<<std::endl;
     std::cout<<b<<std::endl;
     std::cout<<c<<std::endl;
+
+    std::cout<<std::fixed<<std::setprecision(10)<<c<<std::endl;
 
     return 0;
 }
