b67b7d0e1f409af3c18d86721380bee46340acd1
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-03-27 20:17:32 -0300

    testes de operações sem var

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 #include<iostream>
-#include<iomanip>
 
 int main(){
 
-    int a = 8; //int número inteiro
-    float b = 9.6; //float número de ponto flutuante "pega até 7 casas decimais" ocupa 4bytes
-    double c = 4.6654165413; //double ocupa 8bytes "pega até mais números pós ponto"
-
-
-    std::cout<<a<<std::endl;
-    std::cout<<b<<std::endl;
-    std::cout<<c<<std::endl;
-
-    std::cout<<std::fixed<<std::setprecision(10)<<c<<std::endl;
+    std::cout<<8+2<<std::endl;
+    std::cout<<10-4<<std::endl;
+    std::cout<<4*3<<std::endl;
+    std::cout<<8/2<<std::endl;
 
     return 0;
 }
