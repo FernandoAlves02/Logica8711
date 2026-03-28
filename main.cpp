de3164652d5ec20c891cee652dea3d5af49340de
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-03-27 21:22:15 -0300

    correção

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(){
     std::string nome = "Fernando";
 
     std::cout<<"Meu nome é "<<nome<<"."<<std::endl;
-    std::cout<<"Minha altura é "<<altura<<"."<<std::endl;
+    std::cout<<"Minha altura é "<<altura<<"m."<<std::endl;
     std::cout<<"Minha idade é "<<idade<<" anos."<<std::endl;
 
     return 0;
