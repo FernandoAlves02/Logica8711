165889e80f2828c8c266323c21216529573102df
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-03-30 20:27:40 -0300

    comparação de variáveis

--- main.cpp
+++ main.cpp
@@ -1,25 +1,19 @@
 #include<iostream>
-#include<string>
 
 int main(){
 
-    std::string nome;
-    int idade;
-    float altura;
+   
+    int a, b;
 
+    std::cout<<"Digite um valor para A: "<<std::endl;
+    std::cin>>a;
 
-    std::cout<<"Qual seu nome?"<<std::endl; //Solicitação para o cliente informar o nome, detalhe na troca de >> para quando for um insert de informação
-    std::cin>>nome;
+    std::cout<<"Digite um valor para B: "<<std::endl;
+    std::cin>>b;
 
-    std::cout<<"Qual sua idade?"<<std::endl;
-    std::cin>>idade;
+    std::cout<<(a > b)<<std::endl;
+    std::cout<<(a >= b)<<std::endl;
 
-    std::cout<<"Qual sua altura?"<<std::endl;
-    std::cin>>altura;
-
-    std::cout<<"Bem-vindo(a), "<<nome<<"."<<std::endl;
-    std::cout<<"Atualizado o seu registro com a idade de "<<idade<<" anos."<<std::endl;
-    std::cout<<"Registramos também, a sua altura atual de "<<altura<<"m."<<std::endl;
 
     return 0;
 }
