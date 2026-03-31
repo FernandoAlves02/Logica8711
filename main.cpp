2a7e87c499d3c1516fb7b6ac1ca0ea753398f433
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-03-30 21:24:39 -0300

    módulos com comment

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(){
     int a = 8;
     int b = 3;
 
-    std::cout<<(a % b)<<std::endl;
+    std::cout<<(a % b)<<std::endl; // vai sobrar o restante ex. 8 / 3 = 2 e vai sobrar mais 2
 
     return 0;
 }
