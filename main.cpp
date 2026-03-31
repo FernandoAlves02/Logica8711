9436bf6ad67ebaf12fd6023ec32fd8ebe30db636
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-03-30 21:50:57 -0300

    +=

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@ int main(){
    
     int a = 10;
 
-    std::cout<<(++a)<<std::endl;
-    std::cout<<(--a)<<std::endl;
+    a += 5; // a = a + 5
+
+    std::cout<<a<<std::endl;
     
     return 0;
 }
