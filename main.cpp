0ec18c7aeac0887b702b8f53a92bf26cb7203d29
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-01 21:37:07 -0300

    diferença de endl e \n

--- main.cpp
+++ main.cpp
@@ -6,13 +6,25 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int result1 = 4 + 4 * 2;
-    int result2 = 4 + 2 / 2;
-    int result3 = (4 + 4) * 2;
+    int num1, num2, soma;
 
-    std::cout<<result1<<std::endl;
-    std::cout<<result2<<std::endl;
-    std::cout<<result3<<std::endl;
+    std::cout<<"Digite num 1: "<<"\n";
+    std::cin>>num1;
+
+    std::cout<<"Digite num 2: "<<"\n";
+    std::cin>>num2;
+
+    soma = num1 + num2;
+
+    std::cout<<"O resultado da soma é "<<soma<<"\n";
+
+    // "\n" (New Line): Apenas insere o caractere de nova linha. 
+    // É como apertar "Enter" em um editor de texto. 
+    // O computador guarda essa informação em um espaço temporário (chamado buffer) e 
+    // só mostra na tela quando esse espaço enche ou o programa acaba. 
+
+    // std::endl (End Line): Insere a nova linha E força o "esvaziamento" do buffer (operação chamada de flush). 
+    // Ele obriga o computador a exibir tudo o que está guardado na memória imediatamente na tela. 
 
     return 0;
 }
