0e2c4a9abd2e421f05f3c0d98d59ac740a81c787
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-06 20:04:43 -0300

    adição de 1 string a outra ('Texto1' + ' ' + 'texto2')

--- main.cpp
+++ main.cpp
@@ -1,30 +1,16 @@
 #include<iostream>
 #include<windows.h>
+#include<string>
 
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int num1, num2, soma;
+    std::string nome = "Fernando";
+    std::string sobrenome = "Alves";
 
-    std::cout<<"Digite num 1: "<<"\n";
-    std::cin>>num1;
-
-    std::cout<<"Digite num 2: "<<"\n";
-    std::cin>>num2;
-
-    soma = num1 + num2;
-
-    std::cout<<"O resultado da soma é "<<soma<<"\n";
-
-    // "\n" (New Line): Apenas insere o caractere de nova linha. 
-    // É como apertar "Enter" em um editor de texto. 
-    // O computador guarda essa informação em um espaço temporário (chamado buffer) e 
-    // só mostra na tela quando esse espaço enche ou o programa acaba. 
-
-    // std::endl (End Line): Insere a nova linha E força o "esvaziamento" do buffer (operação chamada de flush). 
-    // Ele obriga o computador a exibir tudo o que está guardado na memória imediatamente na tela. 
+    std::cout<<nome + " " + sobrenome<<std::endl;
 
     return 0;
 }
