1fb12871c6b98a0929c379dd255b59d4ad67da95
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-06 21:53:40 -0300

    trocando um caracter usando o índice da var

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@ int main(){
     std::cout<<nome[0]<<std::endl;
     std::cout<<nome[2]<<std::endl;
     std::cout<<nome[nome.length() - 1]<<std::endl;
-    
+    nome[0] = 'A';
+    std::cout<<nome<<std::endl;
+
     return 0;
 }
