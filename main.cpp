ae3c5934717d2d1f8446b04807b6033253670cff
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-06 21:44:38 -0300

    usando [nome.length() - 1]

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@ int main(){
 
     std::string nome = "Fernando";
     
-    std::cout<<nome[0];
-
+    std::cout<<nome[0]<<std::endl;
+    std::cout<<nome[2]<<std::endl;
+    std::cout<<nome[nome.length() - 1]<<std::endl;
+    
     return 0;
 }
