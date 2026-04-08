28c6f912d24c85eeb3adde72b2ed246f6bdfa327
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-08 20:34:02 -0300

    primeiro uso da #include(cmath)

--- main.cpp
+++ main.cpp
@@ -1,22 +1,17 @@
 #include<iostream>
 #include<windows.h>
 #include<string>
+#include<cmath>
 
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    std::string nome = "Fernando";
-    
-    std::cout<<nome[0]<<std::endl;
-    std::cout<<nome[2]<<std::endl;
-    std::cout<<nome[nome.length() - 1]<<std::endl;
-    nome[0] = 'A';
-    std::cout<<nome<<std::endl;
-    std::cout<<nome.at(0)<<std::endl;
-    nome.at(0) = 'H';
-    std::cout<<nome;
+    int a = 5;
+    int b = 3;
+
+    std::cout<<std::max(a, b)<<std::endl;
 
     return 0;
 }
