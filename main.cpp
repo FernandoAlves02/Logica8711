e501e738ebc914e2a856b6db686fb980d840de94
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-08 21:46:17 -0300

    usando if para checar usuário e senha 'string' somente

--- main.cpp
+++ main.cpp
@@ -7,13 +7,19 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int a = 5;
-    int b = 2;
+    std::string user;
+    std::string senha;
 
-    if (a > b){
-        std::cout<<"A é maior que B."<<std::endl;
+    std::cout<<"Digite seu usuário de acesso> "<<std::endl;
+    std::cin>>user;
+
+    std::cout<<"Digite sua senha: "<<std::endl;
+    std::cin>>senha;
+
+    if(senha == "123456789"){
+        std::cout<<"O usuário "<<user<<" está logado com sucesso!!"<<std::endl;
     }else{
-        std::cout<<"B é maior ou igual a A."<<std::endl;
+        std::cout<<"Usuário ou senha inválidos!"<<std::endl;
     }
     
 
