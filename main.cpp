dc35ddcbd1df7fe00c9c74428450f03847418a98
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-13 19:10:59 -0300

    if/else para idade

--- main.cpp
+++ main.cpp
@@ -7,19 +7,15 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    std::string user;
-    std::string senha;
+    int idade;
 
-    std::cout<<"Digite seu usuário de acesso> "<<std::endl;
-    std::cin>>user;
+    std::cout<<"Digite a sua idade: "<<std::endl;
+    std::cin>>idade;
 
-    std::cout<<"Digite sua senha: "<<std::endl;
-    std::cin>>senha;
-
-    if(senha == "123456789"){
-        std::cout<<"O usuário "<<user<<" está logado com sucesso!!"<<std::endl;
+    if(idade >= 18){
+        std::cout<<"Você é maior de idade!"<<std::endl;
     }else{
-        std::cout<<"Usuário ou senha inválidos!"<<std::endl;
+        std::cout<<"Você é menor de idade!!"<<std::endl;
     }
     
 
