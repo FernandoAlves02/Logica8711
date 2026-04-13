0fb187845ca805969129363f89fca45635d8a3f0
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-13 19:46:25 -0300

    else if

--- main.cpp
+++ main.cpp
@@ -12,10 +12,18 @@ int main(){
     std::cout<<"Digite a sua idade: "<<std::endl;
     std::cin>>idade;
 
-    if(idade >= 18){
-        std::cout<<"Você é maior de idade!"<<std::endl;
+    if(idade < 0){
+        std::cout<<"Idade inválida"<<std::endl;
+    }else if(idade <= 2){
+        std::cout<<"Você é um bebê!"<<std::endl;
+    }else if(idade <= 8){
+        std::cout<<"Você é uma criança!"<<std::endl;
+    }else if(idade <= 16){
+        std::cout<<"Você é um(a) adolescente!"<<std::endl;
+    }else if(idade <= 60){
+        std::cout<<"Você é adulto(a)!"<<std::endl;
     }else{
-        std::cout<<"Você é menor de idade!!"<<std::endl;
+        std::cout<<"Você é um(a) sênior!"<<std::endl;
     }
     
 
