9293fdbe6a3e5e6e53ebcce036ce1484ed2fc851
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-15 19:44:28 -0300

    usando #include<thread> e <chrono> para colocar um tempo entre a execução do for

--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include<iostream>
 #include<windows.h>
-#include<string>
+#include<thread> //para Lidar com o tempo de execução
+#include<chrono> //para definir unidades de tempo (ms, segundos)
 
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    //algoritmo que solicite o nome, altura, idade
-    //a idade, iremos classificar <=10 Infantil, <=18 = Adolescente, <= 60 Adulto
-
     for(int i = 10; i >= 0; i--){
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
         std::cout<<"A bomba irá explodir em.."<<i<<std::endl;
     }
+
+    
 
     return 0;
 }
