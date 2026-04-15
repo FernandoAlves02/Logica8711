e90b5053624ab24267b1312f7be78a67a8248a31
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-15 20:17:12 -0300

    usando switch

--- main.cpp
+++ main.cpp
@@ -1,25 +1,41 @@
 #include<iostream>
 #include<windows.h>
-#include<thread> //para Lidar com o tempo de execução
-#include<chrono> //para definir unidades de tempo (ms, segundos)
 
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    for(int i = 0; i <= 10; i++){
-        for(int j = 0; j <= 10; j++){
-            int tabuada = j;
-            if(j == 0){
-                std::cout<<"Abaixo, temos a tabuada do "<<i<<":"<<std::endl;
-            }
-            std::cout<<"O resultado de "<<tabuada<<" x "<<i<<" = "<<i*tabuada<<std::endl;
-            
-        }
-    }
+    int opcao;
 
-    
+    std::cout<<"Digite um valor para opção: "<<std::endl;
+    std::cin>>opcao;
+
+    switch(opcao){
+        case 1:
+            std::cout<<"Domingo!"<<std::endl;
+            break;
+        case 2:
+            std::cout<<"Segunda-feira!"<<std::endl;
+            break;
+        case 3:
+            std::cout<<"Terça-feira!"<<std::endl;
+            break;
+        case 4:
+            std::cout<<"Quarta-feira!"<<std::endl;
+            break;
+        case 5:
+            std::cout<<"Quinta-feira!"<<std::endl;
+            break;
+        case 6:
+            std::cout<<"Sexta-feira!"<<std::endl;
+            break;
+        case 7:
+            std::cout<<"Sábado!"<<std::endl;
+            break;
+        default:
+            std::cout<<"Número inválido!"<<std::endl;
+    }
 
     return 0;
 }
