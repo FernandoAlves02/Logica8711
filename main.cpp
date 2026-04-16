5c9dbf2023c12dee8eefb3a46d96dfc5433ae7cb
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-15 21:29:13 -0300

    diferença de i++ e ++i

--- main.cpp
+++ main.cpp
@@ -6,13 +6,22 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    for(int i = 1; i <= 2; ++i){
-        std::cout<<"Externo: "<<i<<std::endl;
+    // for(int i = 1; i <= 2; ++i){
+    //     std::cout<<"Externo: "<<i<<std::endl;
 
-        for(int j = 1; j <= 3; ++j){
-            std::cout<<"Interno: "<<j<<std::endl;
-        }
-    }
+    //     for(int j = 1; j <= 3; ++j){
+    //         std::cout<<"Interno: "<<j<<std::endl;
+    //     }
+    // }
+
+    int i = 10;
+    int x = i++;
+
+    int j = 10;
+    int y = ++j;
+
+    std::cout<<x<<std::endl;
+    std::cout<<y<<std::endl;
 
     return 0;
 }
