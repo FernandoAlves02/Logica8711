5fddaf3b94750c29f6ac35b8ab0f073c05e87e2b
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-15 21:08:12 -0300

    for com soma do index

--- main.cpp
+++ main.cpp
@@ -6,36 +6,21 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int opcao;
+    int soma = 0;
 
-    std::cout<<"Digite um valor para opção: "<<std::endl;
-    std::cin>>opcao;
-
-    switch(opcao){
-        case 1:
-            std::cout<<"Domingo!"<<std::endl;
-            break;
-        case 2:
-            std::cout<<"Segunda-feira!"<<std::endl;
-            break;
-        case 3:
-            std::cout<<"Terça-feira!"<<std::endl;
-            break;
-        case 4:
-            std::cout<<"Quarta-feira!"<<std::endl;
-            break;
-        case 5:
-            std::cout<<"Quinta-feira!"<<std::endl;
-            break;
-        case 6:
-            std::cout<<"Sexta-feira!"<<std::endl;
-            break;
-        case 7:
-            std::cout<<"Sábado!"<<std::endl;
-            break;
-        default:
-            std::cout<<"Número inválido!"<<std::endl;
+    for(int i = 1; i <= 5; i++){
+        soma += i;
     }
+
+
+    // soma = soma + i
+    // 0    = 0    + 1
+    // 1    = 1    + 2
+    // 3    = 3    + 3
+    // 6    = 6    + 4
+    // 10   = 10   + 5
+
+    std::cout<<"Soma é: "<<soma<<std::endl;
 
     return 0;
 }
