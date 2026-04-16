2aaf99d19fafbd2eb8be99108fd610791408aabb
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-15 21:37:50 -0300

    primeira observação de for it

--- main.cpp
+++ main.cpp
@@ -6,21 +6,9 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    // for(int i = 1; i <= 2; ++i){
-    //     std::cout<<"Externo: "<<i<<std::endl;
-
-    //     for(int j = 1; j <= 3; ++j){
-    //         std::cout<<"Interno: "<<j<<std::endl;
-    //     }
-    // }
-
-    for(int i = 0; i <= 10; i++){
-        std::cout<<"Abaixo, temos a tabuada do "<<i<<":"<<std::endl;
-        for(int j = 0; j <= 10; j++){
-            int tabuada = j;
-            std::cout<<"O resultado de "<<tabuada<<" x "<<i<<" = "<<i*tabuada<<std::endl;
-            
-        }
+    int myNumbers[5] = {10, 20, 30, 40, 50};
+    for(int num : myNumbers){
+        std::cout<<num<<std::endl;
     }
 
     return 0;
