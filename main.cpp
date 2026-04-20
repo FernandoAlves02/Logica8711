d124a0b7aafc2f2727eb5a9c8cfdee538d1fba47
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-20 19:52:37 -0300

    usando size para pegar tamanho do array

--- main.cpp
+++ main.cpp
@@ -7,10 +7,14 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    std::string nomes[3] = {"João", "Paula", "Murilo"};
-    int idades[3] = {27, 31, 20};
+    int numeros[6] = {10, 20, 30, 40, 50, 60};
+    int tamanho = std::size(numeros);
 
-    std::cout<<nomes[1]<<" tem "<<idades[1]<<" anos!"<<std::endl;
+    for(int i = 0; i < tamanho; i++){
+        std::cout<<numeros[i]<<std::endl;
+    }
+
+
 
     return 0;
 }
