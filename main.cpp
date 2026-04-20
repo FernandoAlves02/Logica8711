f72f70064c5288057bfad3805357b49bc6ac587b
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-20 20:10:23 -0300

    usando sizeof para pegar tamanho do array(se menor que versão 16 do c++)

--- main.cpp
+++ main.cpp
@@ -8,11 +8,15 @@ int main(){
     SetConsoleCP(CP_UTF8);
 
     std::string carros[5] = {"Volvo", "BMW", "Ford", "Chevrolet", "Fiat"};
+    int tamanho = sizeof(carros) / sizeof(carros[0]); //tamanho deve voltar 5 que é o tamanho do array, sizeof retorna bytes
+    
+    std::cout<<tamanho<<std::endl;
 
     for(int i = 0; i < std::size(carros); i++){
         std::cout<<carros[i]<<std::endl;
     }
 
+
+
     return 0;
 }
-
