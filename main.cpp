691c2a10bd6a9da152ca11f743da5c6888eade26
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-20 21:00:26 -0300

    array com linha e coluna

--- main.cpp
+++ main.cpp
@@ -7,15 +7,12 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int numeros[3];
-    numeros[0] = 10;
-    numeros[1] = 20;
-    numeros[2] = 30;
-    
-    for(int i = 0; i < std::size(numeros); i++){
-        std::cout<<numeros[i]<<std::endl;
-    }
+    std::string letras[2][4] = { //primeiro linha, depois, coluna
+        {"A", "B", "C", "D"},
+        {"E", "F", "G", "H"}
+    };
 
+    std::cout<<letras[1][1]<<std::endl;
 
     return 0;
 }
