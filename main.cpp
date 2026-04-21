336501b1f7ce9de855c53fb47455daa3d92641e2
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-20 21:48:43 -0300

    primeira visão de do while

--- main.cpp
+++ main.cpp
@@ -7,14 +7,18 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int contagem = 10;
+    int num;
 
-    while(contagem > 0){
-        std::cout<<contagem<<std::endl;
-        contagem--;
+    do{
+        std::cout<<"Digite um valor para num: "<<std::endl;
+        std::cin>>num;
+
+        if(num < 0 || num > 10){
+            std::cout<<"Entrada inválida!!"<<std::endl;
+        }
+    }while(num <= 10 && num >= 0);{
+        std::cout<<"Obrigado!! Você escolheu o número: "<<std::endl;
     }
-
-    std::cout<<"Feliz ano novo!!"<<std::endl;
 
     return 0;
 }
