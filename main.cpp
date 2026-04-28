5b6fd9460863e45f8b9697129436580ad60a6b3a
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-28 19:21:37 -0300

    loop for com break

--- main.cpp
+++ main.cpp
@@ -7,17 +7,11 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int num;
-
-    do{
-        std::cout<<"Digite um valor para num: "<<std::endl;
-        std::cin>>num;
-
-        if(num < 0 || num > 10){
-            std::cout<<"Entrada inválida!!"<<std::endl;
+    for(int i = 0; i < 10; i++){
+        if(i == 4){
+            break;
         }
-    }while(num <= 10 && num >= 0);{
-        std::cout<<"Obrigado!! Você escolheu o número: "<<std::endl;
+        std::cout<<i<<std::endl;
     }
 
     return 0;
