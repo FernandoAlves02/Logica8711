b21a682970a41ddfe504406a8aa32f817f4a83d9
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-04-28 21:07:18 -0300

    dando cout dentro do void, mas os parametros no main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 #include<windows.h>
 
-void minhaFuncao(std::string nome){
-    std::cout<<nome<<std::endl;
+void minhaFuncao(std::string nome, int idade){
+    std::cout<<"Meu nome é "<<nome<<" e tenho "<<idade<<" anos."<<std::endl;
 }
 
 int main(){
@@ -10,8 +10,7 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    minhaFuncao("Fernando");
-    minhaFuncao("Nome");
+    minhaFuncao("Fernando", 24);
 
     return 0;
 }
