5a620d6cdf3dbf6b355994d45cc0da953cd969a3
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-05-07 19:29:26 -0300

    geline

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 #include<windows.h>
 
 int main(){
@@ -6,21 +7,12 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int num;
+    std::string nomeCompleto;
 
-    std::cout<<"Vamos descobrir se o seu número é par ou ímpar?"<<std::endl;
-    std::cout<<"Digite seu número: "<<std::endl;
-    std::cin>>num;
+    std::cout<<"Digite o nome completo do aluno: "<<std::endl;
+    std::getline(std::cin, nomeCompleto);
 
-
-    if(num < 0){
-        std::cout<<"Por favor, digite um número positivo."<<std::endl;
-    }else if(num % 2 == 0){
-            std::cout<<"Parabéns, seu número é par!"<<std::endl;
-        }else{
-            std::cout<<"Dessa vez, seu número é ímpar."<<std::endl;
-        }
-    
+    std::cout<<nomeCompleto<<std::endl;
 
 
     return 0;
