80e14738de652cc3efa51ad79bbbb0e646c71da3
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-05-12 19:29:12 -0300

    a melhor calculadora do mundo 2.0

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int num1, num2, soma, subt, mult;
+    int num1, num2, soma, subt, mult, op;
     float div;
 
     std::cout<<"Digite aqui o valor para o número 1: "<<std::endl;
@@ -15,15 +15,33 @@ int main(){
     std::cout<<"DIgite aqui o valor para o número 2: "<<std::endl;
     std::cin>>num2;
 
-    soma = num1 + num2;
-    subt = num1 - num2;
-    mult = num1 * num2;
-    div = num1 / num2;
+    std::cout<<"----- Escolha a operação -----"<<std::endl;
+    std::cout<<"1 - Soma"<<std::endl;
+    std::cout<<"2 - Subtração"<<std::endl;
+    std::cout<<"3 - Multiplicação"<<std::endl;
+    std::cout<<"4 - Divisão"<<std:: endl;
+    std::cin>>op;
 
-    std::cout<<"O resultado da soma é: "<<soma<<std::endl;
-    std::cout<<"O resultado da subtração é: "<<subt<<std::endl;
-    std::cout<<"O resultado da multiplicação é: "<<mult<<std::endl;
-    std::cout<<"O resultado da divisão é: "<<div<<std::endl;
+    switch(op){
+        case 1:
+            soma = num1 + num2;
+            std::cout<<"O resultado da soma é: "<<soma<<std::endl;
+            break;
+        case 2:
+            subt = num1 - num2;
+            std::cout<<"O resultado da subtração é: "<<subt<<std::endl;
+            break;
+        case 3:
+            mult = num1 * num2;
+            std::cout<<"O resultado da multiplicação é: "<<mult<<std::endl;
+            break;
+        case 4:
+            div = num1 / num2;
+            std::cout<<"O resultado da divisão é: "<<div<<std::endl;
+            break;
+        default:
+            std::cout<<"Escolha inválida!"<<std::endl;
+    };
 
 
     return 0;
