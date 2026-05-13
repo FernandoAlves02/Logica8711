9ca65593844b9b9ebe5a1b803eb488982ac4b5c8
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-05-12 21:48:05 -0300

    Adivinhe o numero!Mais um exemplo de do while com if/else em 1 linha somente

--- main.cpp
+++ main.cpp
@@ -1,71 +1,21 @@
 #include<iostream>
 #include<windows.h>
 
-double saldo = 1000.0;
-std::string titular = "Fernando";
-
-void exibirSaldo(){
-    std::cout<<titular<<" --- Saldo: R$ "<<saldo<<std::endl;
-}
-
-void depositar(double valor){
-    if(valor > 0){
-        saldo += valor;
-        std::cout<<"Depósito realizado com sucesso!"<<std::endl;
-    }else{
-        std::cout<<"Valor inválido!"<<std::endl;
-    }
-}
-
-void sacar(double valor){
-    if(valor > 0 && valor <= saldo){
-        saldo -= valor;
-        std::cout<<"Saldo insuficiente ou valor inválido!"<<std::endl;
-    }
-}
-
-void exibirMenu(){
-    std::cout<<"\n === MENU CONTA BANCÁRIA ==="<<std::endl;
-    std::cout<<"1 - Ver saldo"<<std::endl;
-    std::cout<<"2 - Depósito"<<std::endl;
-    std::cout<<"3 - Sacar"<<std::endl;
-    std::cout<<"4 - Sair"<<std::endl;
-    std::cout<<"Digite uma opção: "<<;
-}
-
-
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int opcao;
-    double valor;
-    do{
-        exibirMenu();
-        std::cin>>opcao;
+    int numero, sorteado = 42;
 
-        switch(opcao){
-            case 1:
-                exibirSaldo();
-                break;
-            case 2:
-                std::cout<<"Digite o valor a depositar: "<<std::endl;
-                std::cin>>valor;
-                depositar(valor);
-                break;
-            case 3:
-                std::cout<<"Digite o valor a sacar: ";
-                std::cin>>valor;
-                sacar(valor);
-                break;
-            case 4:
-                std::cout<<"Saindo da conta.."<<std::endl;
-                break;
-            default:
-                std::cout<<"Opção inválida!"<<std::endl;
-        }
-    }while(opcao != 4);
+    do{
+        std::cout<<"Advinhe: ";
+        std::cin>>numero;
+        if(numero < sorteado) std::cout<<"Maior!"<<std::endl;
+        else if(numero > sorteado) std::cout<<"Menor!"<<std::endl;
+    }while(numero != sorteado);
+
+    std::cout<<"Acertou!!"<<std::endl;
 
     return 0;
 }
