1020fd74b36f264eafe475d8830f70c83b6f791c
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-05-26 19:44:39 -0300

    fatorial de um número + exemplo usando 'long long' que seria um int só que com mais espaço

--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
 #include<iostream>
 #include<windows.h>
-#include<format>
-#include<string>
+
+long long calcularFatorial(int n){
+        long long resultado = 1;
+
+        for(int i = 1; i <= n; i++){
+            resultado *= i;
+        }
+
+        return resultado;
+    }
 
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    std::string nome = "Eduardo";
+    int numero;
 
-    std::string mensagem = std::format("Olá, {}! Você tem {} anos.\n", nome);
-    std::cout<<mensagem;
+    std::cout<<"Digite um número para calcular fatorial: "<<std::endl;
+    std::cin>>numero;
+
+    if(numero < 0){
+        std::cout<<"Erro: fatorial de número negativo não existe!"<<std::endl;
+    }else{
+        long long fatorial = calcularFatorial(numero);
+        std::cout<<numero<<"! = "<<fatorial<<std::endl;
+    }
 
     return 0;
 }
