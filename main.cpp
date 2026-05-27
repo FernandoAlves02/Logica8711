826a586f30f7ad5752e992cc49b8fd74f531948a
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-05-26 21:02:21 -0300

    mais um forzinho, usando array

--- main.cpp
+++ main.cpp
@@ -1,43 +1,21 @@
 #include<iostream>
 #include<windows.h>
 
-void imprimirAteN(int num){
-    if(num == 0){
-        std::cout<<"0! Finalizado! (Da próxima vez, tente um número maior para ficar mais legal)"<<std::endl;
-    }else if(num > 0){
-        for(int i = 0; i <= num; i++){
-            
-            if(i == num){
-                std::cout<<i<<" chegamos no seu número! Obrigado por utilizar o nosso script!"<<std::endl;
-            }else{
-                std::cout<<i<<std::endl;
-            }
-
-        };
-    }else if(num < 0){
-        for(int i = 0; i >= num; i--){
-
-            if(i == num){
-                std::cout<<i<<" chegamos no seu número! Obrigado por utilizar o nosso script!"<<std::endl;
-            }else{
-                std::cout<<i<<std::endl;
-            }
-        };
-    };
-}
-
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int num;
+    int numeros[5];
+    int soma = 0;
 
-    std::cout<<"----- Bem vindo ao nosso mais famoso script, Contamos Para Você!! -----"<<std::endl;
-    std::cout<<"Digite um número que contaremos do zero até chegar nele!"<<std::endl;
-    std::cin>>num;
+    for(int i = 0; i < 5; i++){
+        std::cout<<"Digite o número: "<<(i + 1)<<": ";
+        std::cin>>numeros[i];
+        soma += numeros[i];
+    }
 
-    imprimirAteN(num);
+    std::cout<<"Soma: "<<soma<<std::endl;
 
     return 0;
 }
