0b6f2fc126a9b0e5fc15e2ca61eb39b39e05c3b4
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-05-26 21:42:28 -0300

    ehPrimo?

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
 #include<windows.h>
 
-int encontrarMaior(int arr[], int tamanho){
-    int maior = arr[0];
+bool ehPrimo(int numero){
+    if(numero <= 1){
+        return false;
+    }
 
-    for(int i = 1; i < tamanho; i++){
-        if(arr[i] > maior){
-            maior = arr[i];
+    for(int i = 2; i < numero; i++){
+        if(numero % i == 0){
+            return false;
         }
     }
-    
-    return maior;
+
+    return true;
 }
 
 int main(){
@@ -18,14 +20,16 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int numeros[5];
-    std::cout<<"Digite 5 números: "<<std::endl;
-        for(int i = 0; i < 5; i++){
-            std::cout<<"Digite o número "<<(i + 1)<<": ";
-            std::cin>>numeros[i];
-        }
+    int numero;
+    
+    std::cout<<"Digite um número: "<<std::endl;
+    std::cin>>numero;
 
-    std::cout<<"Maior número: "<<encontrarMaior(numeros, 5)<<std::endl;
+    if(ehPrimo(numero)){
+        std::cout<<numero<<" é primo!"<<std::endl;
+    }else{
+        std::cout<<numero<<" não é primo!"<<std::endl;
+    }
 
     return 0;
 }
