484e2f405bced3ac692765d848d75705ca0e8164
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-05-28 21:57:48 -0300

    mais um exmplinho de recursão, com array dessa vez

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include<iostream>
 #include<windows.h>
 
-void tabuada(int numero, int multiplicador){
-    if(multiplicador == 0) return;
+int somarVetor(int vetor[], int indice){
+    if(indice == 0) return vetor[0];
 
-    tabuada(numero,multiplicador - 1);
-
-    std::cout<<numero<<" x "<<multiplicador<<" = "<<(numero * multiplicador)<<std::endl;
+    return vetor[indice] + somarVetor(vetor, indice - 1);
 }
 
 int main(){
@@ -14,16 +12,9 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int numero = 0;
-    int multiplicador = 0;
+    int numeros[5] = {10, 20, 30, 40, 50};
 
-    std::cout<<"Digite o número que quer terminar a tabuada: "<<std::endl;
-    std::cin>>numero;
-
-    std::cout<<"Digite por quanto deseja multiplicar esse número: "<<std::endl;
-    std::cin>>multiplicador;
-
-    tabuada(multiplicador, numero);
+    std::cout<<"Soma recursiva: "<<somarVetor(numeros, 4)<<std::endl;
 
     return 0;
 }
