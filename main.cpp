349338355c9629f9b80ca1fd4383f2bd3d9aefc2
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-06-02 19:48:06 -0300

    verificando se é um palindromo a frase inputada

--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
 #include<iostream>
 #include<windows.h>
+#include<string>
+#include<cctype>
 
-int somarVetor(int vetor[], int indice){
-    if(indice == 0) return vetor[0];
+std::string limparString(std::string stringSuja){
 
-    return vetor[indice] + somarVetor(vetor, indice - 1);
+    std::string stringLimpa = "";
+    
+    for(char c : stringSuja){
+        if(!isspace(c)){
+            stringLimpa += c;
+        }
+    }
+
+    for(int i = 0; i < stringLimpa.length(); i++){
+        stringLimpa[i] = std::tolower(stringLimpa[i]);
+    }
+
+    return stringLimpa;
+}
+
+bool ehPalindromo(std::string s){
+    
+    s = limparString(s);
+
+    int inicio = 0;
+    int fim = s.length() - 1;
+
+    while(inicio < fim){
+        if(s[inicio] != s[fim]){
+            return false;
+        }
+        inicio++;
+        fim--;
+    }
+    return true;
 }
 
 int main(){
@@ -12,9 +42,20 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int numeros[5] = {10, 20, 30, 40, 50};
+    std::string palavra;
 
-    std::cout<<"Soma recursiva: "<<somarVetor(numeros, 4)<<std::endl;
+    std::cout<<"Bora ver se a tua palavra é um Palíndromo ou não!"<<std::endl;
+    std::cout<<"Digite a sua palavra: ";
+    std::getline(std::cin>>std::ws, palavra);
+
+    std::cout<<"Verificando..."<<std::endl;
+    
+    if(ehPalindromo(palavra)){
+        std::cout<<"Sim! "<<palavra<<" é um palíndromo!"<<std::endl;
+    }else{
+        std::cout<<"Infelizmente, "<<palavra<<" não é um palíndromo."<<std::endl;
+    }
+    
 
     return 0;
 }
