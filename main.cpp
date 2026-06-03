ca3762504f865036ca89bd8f6ecd7d780cffc3db
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-06-02 21:37:08 -0300

    struct + array

--- main.cpp
+++ main.cpp
@@ -5,35 +5,23 @@
 
 struct Aluno{
     std::string nome;
-    float nota1;
-    float nota2;
+    float nota;
 };
-
-float calcularMedia(Aluno a){
-    return (a.nota1 + a.nota2) / 2;
-}
-
 
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    Aluno a1;
+    Aluno alunos[3]{
+        {"Henrique", 8.5},
+        {"Paula", 9.0},
+        {"Juliana", 7.0}
+    };
 
-    std::cout<<"Bem vindo a nossa calculadora de média das suas notas!"<<std::endl;
-    std::cout<<"Por gentileza, quem gostaria de saber a sua media? (Insira o nome do Aluno): ";
-    std::cin>>a1.nome;
-    std::cout<<"Agora, por gentileza, a sua primeira nota: ";
-    std::cin>>a1.nota1;
-    std::cout<<"Por ultimo, vou precisa da sua segunda nota: ";
-    std::cin>>a1.nota2;
-
-    std::cout<<"Calculado..."<<std::endl;
-    Sleep(3000);
-    float media = calcularMedia(a1);
-
-    std::cout<<a1.nome<<" - Média: "<<media<<std::endl;
+    for(int i = 0; i < 3; i++){
+        std::cout<<alunos[i].nome<<": "<<alunos[i].nota<<std::endl;
+    }
 
     return 0;
 }
