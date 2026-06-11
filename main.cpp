9f16c6227500738eb20eb9d89259bd62e2eb8487
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-06-11 20:26:47 -0300

    Menor multiplo comum

--- main.cpp
+++ main.cpp
@@ -1,17 +1,9 @@
 #include<iostream>
 #include<windows.h>
-#include<string>
 
-void bubbleSort(int* arr, int tamanho){
-    for(int i = 0; i < tamanho - 1; i++){
-        for(int j = 0; j < tamanho - i - 1; j++){
-            if(arr[j] > arr[j + 1]){
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
+int mdc(int a, int b){
+    if(b == 0) return a;
+    return mdc(b, a % b);
 }
 
 int main(){
@@ -19,20 +11,8 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int arr[] = {64 , 34, 25, 12, 22, 11, 90};
-    int tamanho = std::size(arr);
-    std::cout<<"Array orginal: ";
-    for(int i = 0; i < tamanho; i++){
-        std::cout<<arr[i]<<" ";
-    }
-    std::cout<<"\n";
-
-    bubbleSort(arr, tamanho);
-    std::cout<<"Array ordenano: ";
-    for(int i = 0; i < tamanho; i++){
-        std::cout<<arr[i]<<" ";
-    }
-    std::cout<<"\n";
+    std::cout<<"MDC(48, 18) = "<<mdc(48, 18)<<std::endl;
+    std::cout<<"MDC(100, 50) = "<<mdc(100, 50)<<std::endl;
        
     return 0;
 }
