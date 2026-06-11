7c61bef09982e7f61e1322fd1b7f2169b85e469f
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.br>
2026-06-11 19:31:49 -0300

    Torre de hanoi usando recursão

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<windows.h>
+#include<string>
+
+void torreDeHanoi(int n, std::string origem, std::string destino, std::string auxiliar){
+    if(n == 1){
+        std::cout<<"Mover disco "<<origem<<" para "<<destino<<std::endl;
+        return;
+    }
+    torreDeHanoi(n - 1, origem, auxiliar, destino);
+    std::cout<<"Mover disco "<<n<<" de "<<origem<<" para "<<destino<<std::endl;
+    torreDeHanoi(n - 1, auxiliar, destino, origem);
+}
+
+int main(){
+
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    int numDiscos;
+    std::cout<<"Quantos discos: ";
+    std::cin>>numDiscos;
+
+    std::cout<<"\nSequência de movimentos: "<<std::endl;
+    torreDeHanoi(numDiscos, "A", "C", "B");
+
+    std::cout<<"\nTotal de movimentos: "<<(1 << numDiscos) - 1<<std::endl;
+
+    return 0;
+}
