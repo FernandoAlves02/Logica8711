155756920416420b39d97029366fc3c12142c843
Fernando Alves <fernandomalves02@gmail.com>
2026-06-27 12:06:59 -0300

    Exercicio 06 finalizado

--- Exercicios/exercicio06.cpp
+++ Exercicios/exercicio06.cpp
@@ -9,5 +9,22 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
+    int num[10];
+
+    std::cout<<"Bem-vindo, aqui verificamos quantos elementos tem uma lista de 10 números, para começar, digite 10 números."<<std::endl;
+    for(int i = 0; i != 10; i++){
+        std::cout<<"Digite um número: ";
+        std::cin>>num[i];
+    }
+
+    int count = 0;
+    for(int i = 0; i != 10; i++){
+        if(num[i] != 0){
+            count++;
+        }
+    }
+
+    std::cout<<"A lista contém "<<count<<" elementos diferentes de zero."<<std::endl;
+
     return 0;
 }
