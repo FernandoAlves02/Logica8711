d948797a8acc3509d62820e189653061ad6c587e
Fernando Alves <fernandomalves02@gmail.com>
2026-06-27 12:12:36 -0300

    Exercicio 09 finalizado

--- Exercicios/exercicio09.cpp
+++ Exercicios/exercicio09.cpp
@@ -9,5 +9,18 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
+    int num[5];
+
+    std::cout<<"Bem-vindo, aqui verificamos a ordem inversa de uma lista de 5 números, para começar, digite 5 números."<<std::endl;
+    for(int i = 0; i != 5; i++){
+        std::cout<<"Digite um número: ";
+        std::cin>>num[i];
+    }
+
+    std::cout<<"A ordem inversa dos números digitados, é: "<<std::endl;
+    for(int i = 4; i != -1; i--){
+        std::cout<<num[i]<<std::endl;
+    }
+
     return 0;
 }
