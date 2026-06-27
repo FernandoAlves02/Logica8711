df8f5949f8698f8763526f7359973d88f2203483
Fernando Alves <fernandomalves02@gmail.com>
2026-06-27 12:11:04 -0300

    Exercicio 08 finalizado

--- Exercicios/exercicio08.cpp
+++ Exercicios/exercicio08.cpp
@@ -9,5 +9,20 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
+    int num[10];
+
+    std::cout<<"Bem-vindo, aqui verificamos quais números são ímpares em uma lista de 10 números, para começar, digite 10 números."<<std::endl;
+    for(int i = 0; i != 10; i++){
+        std::cout<<"Digite um número: ";
+        std::cin>>num[i];
+    }
+
+    std::cout<<"Os números ímpares digitados são: "<<std::endl;
+    for(int i = 0; i != 10; i++){
+        if(num[i] % 2 != 0){
+            std::cout<<num[i]<<std::endl;
+        }
+    }
+
     return 0;
 }
