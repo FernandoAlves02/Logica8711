1a2e1ad593cf84487e80a7f53cd935ad8176e7b0
Fernando Alves <fernandomalves02@gmail.com>
2026-06-27 13:07:14 -0300

    Exercicio 15 finalizado

--- Exercicios/exercicio15.cpp
+++ Exercicios/exercicio15.cpp
@@ -9,5 +9,27 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
+    int num[10];
+
+    std::cout<<"Bem-vindo, aqui verificamos quantos números positivos e negativos existem em uma lista de 10 números, para começar, digite 10 números."<<std::endl;
+    for(int i = 0; i != 10; i++){
+        std::cout<<"Digite um número: ";
+        std::cin>>num[i];
+    }
+
+    int positivos = 0;
+    int negativos = 0;
+
+    for(int i = 0; i != 10; i++){
+        if(num[i] > 0){
+            positivos++;
+        } else if(num[i] < 0){
+            negativos++;
+        }
+    }
+
+    std::cout<<"Quantidade de números positivos: "<<positivos<<std::endl;
+    std::cout<<"Quantidade de números negativos: "<<negativos<<std::endl;
+
     return 0;
 }
