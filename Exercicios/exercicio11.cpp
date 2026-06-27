bea59d66280f78ef56ed4d18d5b53f6fe77a9fc2
Fernando Alves <fernandomalves02@gmail.com>
2026-06-27 12:15:57 -0300

    Exercicio 11 finalizado

--- Exercicios/exercicio11.cpp
+++ Exercicios/exercicio11.cpp
@@ -9,5 +9,25 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
+    int num[10];
+
+    std::cout<<"Bem-vindo, aqui verificamos se um número específico está presente em uma lista de 10 números, para começar, digite 10 números."<<std::endl;
+    for(int i = 0; i != 10; i++){
+        std::cout<<"Digite um número: ";
+        std::cin>>num[i];
+    }
+
+    int buscar;
+    std::cout<<"Digite o número que deseja buscar: ";
+    std::cin>>buscar;
+
+    for(int i = 0; i != 10; i++){
+        if(num[i] == buscar){
+            std::cout<<"O número "<<buscar<<" foi encontrado na posição "<<i+1<<" da lista."<<std::endl;
+            return 0;
+        }
+    }
+
+    std::cout<<"O número "<<buscar<<" não foi encontrado na lista."<<std::endl;
     return 0;
 }
