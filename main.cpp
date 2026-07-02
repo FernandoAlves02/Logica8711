013a847539770870541195a6c79c8cfb27d88413
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.bR>
2026-07-02 20:27:34 -0300

    array list

--- main.cpp
+++ main.cpp
@@ -1,47 +1,30 @@
 #include<iostream>
 #include<windows.h>
-#include<cctype>
+#include<vector>
 
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int matriz[3][4] = {
-        {10,25,5,40},
-        {15,8,30,12},
-        {20,3,18,35}
-    };
+    std::vector<int>numeros;
 
-    int maior = matriz[0][0];
-    int menor = matriz[0][0];
-    int linhaM = 0, colunaM = 0;
-    int linhaN = 0, colunaN = 0;
+    std::cout<<"===== Acessar elemento ====="<<std::endl;
 
-    std::cout<<"===== Min e Max em Matriz ====="<<std::endl;
+    numeros.push_back(100);
+    numeros.push_back(200);
+    numeros.push_back(300);
+    numeros.push_back(400);
 
-    std::cout<<"Matriz: "<<std::endl;
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 4; j++){
-            std::cout<<matriz[i][j]<<" ";
-
-            if(matriz[i][j] > maior){
-                maior = matriz[i][j];
-                linhaM = i + 1;
-                colunaM = j + 1;
-            }
-            if(matriz[i][j] < menor){
-                menor = matriz[i][j];
-                linhaN = i + 1;
-                colunaN = j + 1;
-            }
-        }
-        std::cout<<std::endl;
+    std::cout<<"Vector: ";
+    for(int i = 0; i< numeros.size(); i++){
+        std::cout<<numeros[i]<<" ";
     }
-    
     std::cout<<std::endl;
-    std::cout<<"Maior: "<<maior<<" na posição ["<<linhaM<<"]["<<colunaM<<"]"<<std::endl;
-    std::cout<<"Menor: "<<menor<<" na posição ["<<linhaN<<"]["<<colunaN<<"]"<<std::endl;
+
+    std::cout<<"Elemento na posição 0: "<<numeros[0]<<std::endl;
+    std::cout<<"Elemento na posição 2: "<<numeros[2]<<std::endl;
+    std::cout<<"Último elemento: "<<numeros[3]<<std::endl;
 
     return 0;
 }
