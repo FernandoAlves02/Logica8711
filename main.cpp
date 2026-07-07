4065f982d4b592aa24132c66c4b11fd669e49327
FERNANDO MOREIRA ALVES <03524592040@senacrs.edu.bR>
2026-07-07 19:18:00 -0300

    Soma de vetor com ternario para deixar bonitin

--- main.cpp
+++ main.cpp
@@ -9,33 +9,22 @@ int main(){
 
     std::vector<int>numeros;
 
-    std::cout<<"===== Maior e menor ====="<<std::endl;
-    
-    numeros.push_back(15);
-    numeros.push_back(8);
-    numeros.push_back(25);
-    numeros.push_back(3);
-    numeros.push_back(42);
+    numeros.push_back(10);
+    numeros.push_back(20);
+    numeros.push_back(30);
+    numeros.push_back(40);
 
-    std::cout<<"Vector: ";
+    std::cout<<"Bora somar um vetor aí!"<<std::endl;
+    std::cout<<"Vetor: ";
+
+    int soma = 0;
     for(int i = 0; i < numeros.size(); i++){
-        std::cout<<numeros[i]<<" ";
+        std::cout<<numeros[i]<<(i == (numeros.size() - 1) ? "." : ", ");
+        soma += numeros[i];
     }
     std::cout<<std::endl;
 
-    int maior = numeros[0];
-    int menor = numeros[0];
-
-    for(int i = 1; i < numeros.size(); i++){
-        if(numeros[i] > maior){
-            maior = numeros[i];
-        }
-        if(numeros[i] < menor){
-            menor = numeros[i];
-        }
-    }
-    std::cout<<"Maior: "<<maior<<std::endl;
-    std::cout<<"Menor: "<<menor<<std::endl;
+    std::cout<<"O montante da soma dos números é de: "<<soma<<std::endl;
 
     return 0;
 }
